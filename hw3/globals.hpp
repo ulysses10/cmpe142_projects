660b38288991af5e1fc729d6c8a3c266bc34c72b
Ulysses <ulysses.villegas@sjsu.edu>
2021-02-25 13:20:59 -0800

    hw3 update

--- hw3/globals.hpp
+++ hw3/globals.hpp
@@ -1,14 +1,16 @@
 
-
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
-const int trials = 2;           // -- number of trials need to run. Change to 5 per assignment requirement
-const int quanta_length = 20;   // -- quanta (time slices)
-const int MAX_PROCESSES = 3;    // -- number of processes to be generated
+const int trials = 1;           // -- number of trials need to run. Change to 5 per assignment requirement
+const double max_run_time = 4.9;
+const int arrival_range = 20;   // -- Time range of cpu access. No process should run if it does not get cpu time. 
+                                // -- Every process that gets cpu time should complete
+const int quanta = arrival_range + 10;   // -- quanta time line. Only processes that get cpu time should be in the quanta
+const int MAX_PROCESSES = 10;    // -- number of processes to be generated
 char id[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T'};
-char quanta[quanta_length];     // -- array to hold processes that ran on CPU
+char timeline[quanta];     // -- array to hold processes that ran on CPU
 
 
 struct process{
@@ -21,50 +23,57 @@ struct process{
 process *generateSchedule()
 {
     static process schedule[MAX_PROCESSES];
-    for(int i=0; i<MAX_PROCESSES; i++)
-    {   
+    for(int i=0; i<MAX_PROCESSES; i++){   
         // Name process
         schedule[i].name = id[i];
 
         // Generate arrival time
-        schedule[i].arrival_time = rand()%100;
+        schedule[i].arrival_time = rand()%arrival_range;
 
         // Generate runtime
-        schedule[i].run_time = (float)rand()/(RAND_MAX)*9.9+.1;
+        schedule[i].run_time = (float)rand()/(RAND_MAX)*max_run_time+.1;
 
         // Generate priority
         schedule[i].priority = rand()%5;
     }
-
+    // -- Sorts by arrival time
+    for( size_t i = 1; i < MAX_PROCESSES; i++){
+        process key = schedule[i];
+        int j=i-1;
+        while(j>=0 && schedule[j].arrival_time>key.arrival_time) {    // -- for non increasing order: a[j]<key
+            schedule[j+1]=schedule[j];
+            j-=1;
+        }
+        schedule[j+1]=key;
+    }
     return schedule;
 }
 
-void printQuantaArray( )
+void printQuantaArray( int offset)
 {
+    // -- 
+    cout << "CPU access time in quantum: " << arrival_range << "\n";
+    cout << "Quanta range: 0 - " <<  arrival_range-1 << "\n";
     // -- Processes in quanta
     cout << setw(20) << "Scheduling Timeline:";
-    for( int i=0; i<quanta_length; i++)
-    {
-        cout << setw(3) << quanta[i] <<"|";
+    for( int i=0; i<arrival_range+offset; i++){
+        cout << setw(2) << right << timeline[i] <<"|";
     }
     // -- Quanta labels
-    cout << "\n" << setw(20) <<"Quanta:";
-    for(int i=0; i<quanta_length; i++)
-    {
-        cout << setw(3) << i <<"|";
+    cout << "\n" << setw(20) << right <<"Quanta:";
+    for(int i=0; i<arrival_range+offset; i++){
+        cout << setw(2) << right << i <<"|";
     }
     cout << "\n\n";
 }
 
-void printSchedule( process schedule[])
-{
-    for( int i=0; i<MAX_PROCESSES; i++)
-    {
+void printSchedule( process schedule[]){
+    for( int i=0; i<MAX_PROCESSES; i++){
 		cout << "process "<< schedule[i].name << " | ";
 
-        cout << "arrival time "<< setw(4) << schedule[i].arrival_time << " | ";
+        cout << "arrival time "<< setw(4) << right << schedule[i].arrival_time << " | ";
 
-        cout << "run time "<< setw(8) << schedule[i].run_time << " | ";
+        cout << "run time "<< setw(8) << left << schedule[i].run_time << " | ";
 
         cout << "priority " << schedule[i].priority << "\n";
     }
--- hw3/psa.hpp
+++ hw3/psa.hpp
@@ -5,61 +5,120 @@
 #include "globals.hpp"
 
 // --First Come First Serve
-void fcfs()//process schedule[])
-{
-    /*
-     * 
-     * This section is only needed for testing since the assignment only asks to output the readyQ
-     * 
-    // -- Setup Random Process schedule
-    process *schedule=generateSchedule();
-    cout << "Processes generated:\n";
-    printSchedule(schedule);            
-    cout << "\n";
- 
-    // Setup ready Queue
-    process readyQ[MAX_PROCESSES];
-    for(int i=0; i<MAX_PROCESSES; i++)
-    {
-        readyQ[i]=schedule[i];
-    }
-    */ // uncomment this^ section and comment out the next line of code to print the random schedule first and then setup readyQ
-    
-    process *readyQ=generateSchedule();  // comment out this line if needed ^
+void fcfs(){   
+    process *readyQ=generateSchedule();  // a schedule sorted by arrival time is generated
 
-    // -- Sorts by arrival time
-    for( size_t i = 1; i < MAX_PROCESSES; i++)
-    {
-        process key = readyQ[i];
-        int j=i-1;
-        while(j>=0 && readyQ[j].arrival_time>key.arrival_time) // -- for non increasing order: a[j]<key
-        {
-            readyQ[j+1]=readyQ[j];
-            j-=1;
-        }
-        readyQ[j+1]=key;
-    }
     cout << "\nSorted Schedule by Arrival Time:\n";
     printSchedule(readyQ);
 
-    // -- Algorithm for FCFS
-    int t=0;
-    for(int i=0; i<MAX_PROCESSES; i++)
-    {
-        while(readyQ[i].run_time>0)
-        {
-            quanta[t] = readyQ[i].name;
-            readyQ[i].run_time--;
-            t++;
+    // -- Algorith for FCFS (actual)
+    int p=0;
+    int offset=0;
+    // -- CPU time
+    for( int i=0; i<arrival_range; i++){
+        if(readyQ[p].arrival_time <= i && p<MAX_PROCESSES){ // -- p cant exceed MAX_PROCESSES (readyQ length)
+            for(int j=0; j< readyQ[p].run_time; j++){
+                timeline[i]=readyQ[p].name;
+                i++;
+            }
+            offset = i - arrival_range; // -- offset to accomadate processes completed outside cpu time
+            i--;
+            p++;
+        }
+        else{
+            timeline[i]='-';
         }
     }
-    // -- Fill unused quanta with hyphens
-    while(t<quanta_length)
-    {
-        quanta[t]='-';
-        t++;
+
+    if(offset<0) offset=0;
+    // -- Print results
+    printQuantaArray(offset);
+}
+
+// -- Shortest Job First
+void sjf(){
+
+    process *readyQ=generateSchedule();  // a schedule sorted by arrival time is generated
+
+    // -- Sorts by shortest job
+    int j=0;
+    for( int i=0; i<MAX_PROCESSES; i++){
+        j=i;
+        while(readyQ[i].arrival_time == readyQ[j+1].arrival_time) // -- get the index range of processes with same arrival time
+            j++;
+        if(j!=i){
+            j++;
+            // sort by run time from i to j-1
+            for( size_t k = i+1; k < j; k++){
+                process key = readyQ[k];
+                int p=k-1;
+                while(p>=i && readyQ[p].run_time>key.run_time) {    // -- for non increasing order: a[j]<key
+                 readyQ[p+1]=readyQ[p];
+                 p-=1;
+            }
+            readyQ[p+1]=key;
+            }
+        }
     }
 
+    cout << "\nSorted Schedule by Arrival and Run Time:\n";
+    printSchedule(readyQ);
+
+    // -- Algorith for FCFS (actual)
+    int p=0;
+    int offset=0;
+    // -- CPU time
+    for( int i=0; i<arrival_range; i++){
+        if(readyQ[p].arrival_time <= i && p<MAX_PROCESSES){ // -- p cant exceed MAX_PROCESSES (readyQ length)
+            for(int j=0; j< readyQ[p].run_time; j++){
+                timeline[i]=readyQ[p].name;
+                i++;
+            }
+            offset = i - arrival_range; // -- offset to accomadate processes completed outside cpu time
+            i--;
+            p++;
+        }
+        else{
+            timeline[i]='-';
+        }
+    }
+
+    if(offset<0) offset=0;
     // -- Print results
-    printQuantaArray();
+    printQuantaArray(offset);
+
+}
+
+// -- Shortest Remaining Time
+void srt(){
+    process *readyQ=generateSchedule();  // a schedule sorted by arrival time is generated
+    cout << "\nSorted Schedule by Arrival Time:\n";
+    printSchedule(readyQ);
+
+    // -- Sorts by shortest job ( shortest remaining time )
+    int j=0;
+    for( int i=0; i<MAX_PROCESSES; i++){
+        j=i;
+        while(readyQ[i].arrival_time == readyQ[j+1].arrival_time) // -- get the index range of processes with same arrival time
+            j++;
+        if(j!=i){
+            j++;
+            // sort by run time from i to j-1 
+            for( size_t k = i+1; k < j; k++){
+                process key = readyQ[k];
+                int p=k-1;
+                while(p>=i && readyQ[p].run_time>key.run_time) {    // -- for non increasing order: a[j]<key
+                 readyQ[p+1]=readyQ[p];
+                 p-=1;
+                }
+            readyQ[p+1]=key;
+            }
+        }
+    }
+
+    // -- Run like sjf but check every quanta if new process comes in. 
+        // if new process has shorter remaining run time, push to front of q ( halt current process and start new process on next quantum)
+        // keep track of current run time for a process to calculate remaining time for comparison
+        // 
+
 }
